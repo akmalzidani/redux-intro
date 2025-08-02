797db847dd78b761a6cf96d35498fcb43f549b2f
akmalzidani <akmal20031003@gmail.com>
2025-08-02 23:53:32 +0700

    Add react-redux for state management and update Customer component to use Redux store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 
 import store from "./store";
 
-store.dispatch({
-  type: "account/deposit",
-  payload: 1000,
-});
-console.log(store.getState());
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
