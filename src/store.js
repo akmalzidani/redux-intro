db69b153c29c3016748471c66478297eb2bd6b36
akmalzidani <akmal20031003@gmail.com>
2025-08-02 17:32:15 +0700

    Add Redux for state management and implement basic store functionality (learning purpose)

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -20,12 +22,15 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) return state;
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case "account/payLoan":
       return {
         ...state,
         loan: 0,
+        loanPurpose: "",
         balance: state.balance - state.loan,
       };
 
@@ -33,3 +38,21 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Butuh Duek",
+  },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
